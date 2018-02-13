c21e96286eef8dc26781c555e27eb425a911e6c2
melmichael <muresan@us.ibm.com>
2018-02-13 05:37:52 -0500

    Added strat script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 // set express view engines
@@ -66,6 +67,6 @@ app.get('/bad', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('Servere is running on port 3000')
+app.listen(port, () => {
+  console.log(`Servere is running on port ${port}`);
 });
